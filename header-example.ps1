01e4e980a93bf6dd3f8d0b5ab108c874926442f6
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2022-03-11 22:23:36 -0600

    Update and rename example.ps1 to header-example.ps1

---
+++ header-example.ps1
@@ -0,0 +1,19 @@
+<# 
+.SYNOPSIS
+    A summary of how the script works and how to use it.
+
+.DESCRIPTION 
+    A long description of how the script works and how to use it.
+ 
+.NOTES 
+    Information about the environment, things to need to be consider and other information.
+
+.COMPONENT 
+    Information about PowerShell Modules to be required.
+
+.LINK 
+    Useful Link to ressources or others.
+ 
+.Parameter ParameterName 
+    Description for a parameter in param definition section. Each parameter requires a separate description. The name in the description and the parameter section must match. 
+#>
